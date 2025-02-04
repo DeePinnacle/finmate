df63aef7996cb540999a299a2219231407b7eb6c
Deen <officialdeepinnacle@gmail.com>
2025-02-04 15:49:59 +0100

    completed the how it work section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import { Button } from "@/components/ui/button";
 import { Crown, PiggyBank } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { features } from "@/data/data";
+import { features, steps } from "@/data/data";
+import { Check } from "lucide-react";
 const Finmate = () => {
   return (
     <>
@@ -104,27 +105,93 @@ const Finmate = () => {
       {/* feature section starts here */}
       <section className="feature-section">
         {/* content */}
-        <div className="w-full px-2 py-6 flex flex-col items-center justify-center">
-          <h1 className="text-3xl text-center">Features <span className="text-finmate_secondary">That Empower Your</span> Finances</h1>
+        <div className="w-full py-6 flex flex-col items-center justify-center">
+          <h1 className="text-3xl text-center">
+            Features{" "}
+            <span className="text-finmate_secondary">That Empower Your</span>{" "}
+            Finances
+          </h1>
           <p className=" my-2 text-lg">What Sets FinMate Apart</p>
           {/* feature contents */}
-          <div>
-            {
-              features?.map((feature)=>(
-                <div key={feature.id} className="w-full bg-finmate_secondary px-2 py-6 my-3">
-                  {< feature.icon className="text-finmate_primary my-3 text-5xl block mx-auto" />}
-                  <p className="text-finmate_primary text-3xl mb-3 text-center">{ feature.title }</p>
-                  <p className="text-finmate_primary text-center text-lg">{ feature.content }</p>
-                </div>
-              ))
-            }
+          <div className="w-full p-2">
+            {features?.map((feature) => (
+              <div
+                key={feature.id}
+                className="w-full bg-finmate_secondary px-2 py-6 my-3"
+              >
+                {
+                  <feature.icon className="text-finmate_primary my-3 text-5xl block mx-auto" />
+                }
+                <p className="text-finmate_primary text-3xl mb-3 text-center">
+                  {feature.title}
+                </p>
+                <p className="text-finmate_primary text-center text-lg">
+                  {feature.content}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </section>
       {/* feature section ends here */}
       {/* How it works section starts here */}
-      <section></section>
+      <section className="w-full">
+        {/* content */}
+        <div className="w-full py-6 flex flex-col items-center justify-center">
+          <h1 className="text-3xl text-center">
+            How <span className="text-finmate_secondary">FinMate</span> Works
+          </h1>
+          <p className=" my-2 text-lg">Your path to Financial Mastery</p>
+          {/* feature contents */}
+          <div className="w-full p-2">
+            {steps?.map((step) => (
+              <div key={step.id} className="w-full px-2 py-6 my-3">
+                <p className="text-finmate_secondary text-2xl">
+                  Step {step.id}: {step.title}
+                </p>
+
+                <p className="my-4 font-light font-inter leading-normal">
+                  {step.description}
+                </p>
+                {/* {
+                  step?.lists.map((list)=>(
+                    <span key={list.id} className="flex flex-row gap-2 mt-2">
+                      <Check className="text-finmate_secondary" />
+                      <p className="text-finmate_secondary my-2">{list.title}: <span>{list.content}</span></p>
+                    </span>
+                  ))
+                } */}
+                <div className="w-full h-72 relative overflow-hidden">
+                  <Image
+                    src={`/${step.image}`}
+                    alt={step.image}
+                    fill
+                    priority
+                    objectFit="cover"
+                    objectPosition="center"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
       {/* ends here */}
+
+      {/* CTA Section starts here */}  
+      <div className="w-full bg-finmate_secondary pb-7">
+        <div className="w-full px-2">
+         {/* cta content */}
+          <div className="w-full py-8">
+            <p className="text-5xl text-finmate_primary font-semibold mb-4">Take Charge of Your Finances Today!</p>
+            <p className="my-3 text-lg">Transform your financial future with FinMate. Join thousands of users who are taking control of their money through smart tracking, dynamic budgeting, and actionable insights.</p>
+            <Link href={""} className="bg-finmate_primary px-2 py-5 h-16 w-3/5 my-3 text-lg text-finmate_secondary font-light flex flex-row items-center justify-center hover:bg-finmate_primary">Get Started for Free</Link>
+          </div>
+          {/* cta image */}
+          <div className="w-full h-96 bg-finmate_primary rounded-lg my-4"></div>
+        </div>
+      </div>
+      {/* CTA Section ends here */}
     </>
   );
 };
--- data/data.ts
+++ data/data.ts
@@ -39,3 +39,113 @@ export const features = [
         content: "Access detailed and visually appealing reports that break down your income, expenses, and savings trends. Make informed decisions with clear financial insights."
     }
 ]
+
+export const steps = [
+    {
+        id: 1,
+        title: "Sign Up & Personalize",
+        description: "Create your account and customize your profile. Set your financial preferences to receive a personalized experience from day one.",
+        image: "reg_profile.png",
+        lists: [
+            {
+                id: 1,
+                title: "Create Your Account",
+                content: "Register using your email or social login."
+            },
+            {
+                id: 2,
+                title: "Set Your Preferences",
+                content: "Choose your preferred currency, language, and notification settings."
+            },
+            {
+                id: 3,
+                title: "Customize Your Dashboard",
+                content: "Tailor the look and feel of your profile for a personalized experience."
+            }
+        ],
+    },
+    {
+        id: 2,
+        title: "Link & Organize Your Finances",
+        description: "Manually input or import your income and expenses. FinMate helps you categorize your transactions, giving you clear visibility into your spending habits.",
+        image: "card.png",
+        lists: [
+            {
+                id: 1,
+                title: "Input Transactions",
+                content: "Manually add your income and expenses for full control."
+            },
+            {
+                id: 2,
+                title: "Categorize Automatically",
+                content: "Use pre-defined categories to organize transactions effortlessly."
+            },
+            {
+                id: 3,
+                title: "Tag Recurring Expenses",
+                content: "Mark regular payments for easy tracking over time."
+            },
+            {
+                id: 4,
+                title: "Review Financial Data",
+                content: "Consolidate all your entries into an organized, easy-to-navigate interface."
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Set Your Budget & Savings Goals",
+        description: "Define dynamic budgets and establish savings milestones tailored to your lifestyle. This step ensures you’re set up for success from the start.",
+        image: "piggy_bank.png",
+        lists: [
+            {
+                id: 1,
+                title: "Create Budget Categories",
+                content: "Define budgets for essentials like groceries, bills, and entertainment."
+            },
+            {
+                id: 2,
+                title: "Establish Savings Targets",
+                content: "Set realistic savings goals that align with your financial priorities."
+            },
+            {
+                id: 3,
+                title: "Schedule Periodic Reviews",
+                content: "Choose weekly or monthly intervals to assess your budget’s performance."
+            },
+            {
+                id: 4,
+                title: "Receive Custom Alerts",
+                content: "Get notifications when you're nearing your budget limits or hitting savings milestones."
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Track, Analyze & Optimize",
+        description: "Monitor your progress through intuitive dashboards and detailed reports. Use these insights to adjust your spending and maximize your savings.",
+        image: "chart_4.png",
+        lists: [
+            {
+                id: 1,
+                title: "Visualize Your Data",
+                content: "Use intuitive dashboards to monitor your spending and savings trends."
+            },
+            {
+                id: 2,
+                title: "Review Detailed Reports",
+                content: "Dive into analytics that break down your income, expenses, and progress over time."
+            },
+            {
+                id: 3,
+                title: "Get Actionable Insights",
+                content: "Receive recommendations to fine-tune your spending habits."
+            },
+            {
+                id: 4,
+                title: "Receive recommendations to fine-tune your spending habits.",
+                content: "Modify your budgets and goals based on real-time feedback to optimize financial health."
+            }
+        ]
+    }
+]
