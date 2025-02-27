f47fd032a53218e66da376f6caf53f47f8e47e1c
Deen <officialdeepinnacle@gmail.com>
2025-02-27 16:45:31 +0100

    started the reset password design

--- app/(user)/reset-password/page.tsx
+++ app/(user)/reset-password/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ResetPassword = () => {
   return (
-    <div>ResetPassword</div>
+    <div className="w-full min-h-screen bg-white px-2 flex flex-col items-center justify-center">
+        <div className="w-full max-w-4xl">
+            Reset Password page
+        </div>
+    </div>
   )
 }
 
